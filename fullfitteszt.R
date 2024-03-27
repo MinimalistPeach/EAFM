a4a77f9f3615405da60e6cc788a7fc57f3efdf0f
MinimalistPeach <stremler.lacika77@gmail.com>
2024-03-27 14:21:45 +0100

    fix

--- fullfitteszt.R
+++ fullfitteszt.R
@@ -1,17 +1,16 @@
-DiceRoll <- function(N,K,p) {
+rolldice <- function(N,K,p)
+{
   sum = 0;
 
-  for(i in 1:length(k) {
-    sum += ((K[i] - N*p[i]) ^ 2) / N*p[i];
+  for(i in 1:length(K)) 
+  {
+    sum = sum + ((K[i] - N*p[i]) ^ 2) / N*p[i];
   }
 
   print(sum);
 }
 
-
-
-
-N <- 600;
-K <- c(83, 91, 122, 107, 74, 123);
-p <- c(1/6, 1/6, 1/6, 1/6, 1/6, 1/6);
-DiceRoll(N, K, p);
+N = 600;
+K = c(83, 91, 122, 107, 74, 123);
+p = c(1/6, 1/6, 1/6, 1/6, 1/6, 1/6);
+rolldice(N,K,p);
